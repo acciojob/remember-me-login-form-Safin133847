0ef20f09cfc658acea171e4ff969c5604876e11e
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-12-01 11:40:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+const loginForm = document.getElementById("login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkboxInput = document.getElementById("checkbox");
+const existingButton = document.getElementById("existing");
+
+const storedUsername = localStorage.getItem("username");
+const storedPassword = localStorage.getItem("password");
+
+if (storedUsername && storedPassword) {
+    existingButton.style.display = "block";
+
+    existingButton.addEventListener("click", function () {
+        alert(`Logged in as ${storedUsername}`);
+    });
+}
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const rememberMe = checkboxInput.checked;
+
+    if (rememberMe) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+    } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+    }
+
+    alert(`Logged in as ${username}`);
+
+    usernameInput.value = '';
+    passwordInput.value = '';
+    checkboxInput.checked = false;
+});
